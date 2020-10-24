9f29c1aae204bb2d5c23fedd6eae313b085ef259
Scott Young <syoung@instructure.com>
2020-10-23 19:43:34 -0600

    add filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { SentryInterceptor } from '../sentry.interceptor';
 import { CreateTaskDto } from './create-task.dto';
+import { GetTasksFilterDto } from './get-tasks-filter.dto';
 
 @UseInterceptors(SentryInterceptor)
 
@@ -11,12 +12,32 @@ export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if(Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
+        
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id)
     }
 
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto)
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+        return this.tasksService.updateTaskStatus(id, status)
+    }
+
+    @Delete()
+    deleteTask(@Param('id') id: string): void {
+        this.tasksService.deleteTask(id)
     }
 }
